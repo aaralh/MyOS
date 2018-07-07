2e755d0a65ed3e9a4d1f62b6381f968393833c8e
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-07-07 08:13:55 +0300

    Added initial port handling

--- kernel.cpp
+++ kernel.cpp
@@ -3,7 +3,7 @@
 #include "screen.h"
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
-    Screen screen = Screen();
+    Screen screen;
     screen.printf("H\ne\nl\nl\no\n \nw\no\nr\nl\nd\n!\n");
     screen.printf("\nH\ne\nl\nl\no\n!\n");
     screen.printf("H\ne\nl\nl\no w\norld!");
@@ -13,6 +13,7 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     screen.printf("Hello World!");
 
     GlobalDescriptorTable gdt;
+
 
     while(1);
 }
